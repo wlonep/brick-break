048b6551da57e31e1fdb14e6d6239f6fc5bf1f31
lewissong <lewis.jsong@gmail.com>
2025-06-07 01:02:00 +0900

    레이저 4개로 수정, 사잇각 20°

--- js/enemy.js
+++ js/enemy.js
@@ -36,7 +36,7 @@ const enemyLaserSpeed = 250; // 레이저 속도 조절(공이 지금 400임)
 let enemyLaserTimer = 0;
 const enemyLaserImg = new Image();
 enemyLaserImg.src = "src/ball/enemyship_bullet.png";
-const laserAngles = [-45, -15, 15, 45].map(deg => deg * Math.PI / 180); // -45°, -15°, 15°, 45°
+const laserAngles = [-30, -10, 10, 30].map(deg => deg * Math.PI / 180); // -30°, -10°, 10°, 30°
 
 
 const asteroidImages = [];
